363c571f7291cdbc0daec9c809ca5aa63450fa5e
Daniel Hughes <daniel.hughes.sweden@gmail.com>
2019-10-29 18:55:39 +0100

    Adding OptionsMenu

--- src/OptionsMenu.java
+++ src/OptionsMenu.java
@@ -1,8 +1,27 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class OptionsMenu{
 
     public static void showOptions(){
+        Game.gameFrame.removeCenterComponent();
+        JPanel menuComponents = new JPanel(new BorderLayout());
+
+        ImagePanel topBottomPanel = new ImagePanel("Graphics/Metal Texture Pattern.jpg", 500, 100);
+        ImagePanel menuPanel = new ImagePanel("Graphics/Metal Background Image.jpg", 400, 300);
+        ImagePanel optionButtonsPanel = new ImagePanel("Graphics/Wooden Background.jpg", 100, 300);
+
+
+        menuComponents.add(topBottomPanel, BorderLayout.NORTH);
+        menuComponents.add(topBottomPanel, BorderLayout.SOUTH);
+        menuComponents.add(menuPanel, BorderLayout.CENTER);
+        menuComponents.add(optionButtonsPanel, BorderLayout.EAST);
+
+
+        Game.gameFrame.backgroundPanel.add(menuComponents);
+        Game.gameFrame.backgroundPanel.revalidate();
+
+
 
 
     }
